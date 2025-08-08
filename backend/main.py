e919c911056d4ff0026f6464d5ed24f0a4d7daf0
Agostina Guevara <agostina.guevara@outlook.com>
2025-08-08 18:07:44 -0300

    camibie el fetch para que pida peticiones a render!

--- backend/main.py
+++ backend/main.py
@@ -21,7 +21,7 @@ app = FastAPI()
 #habilitamos cors para las peticiones del front al back
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://127.0.0.1:5500"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
